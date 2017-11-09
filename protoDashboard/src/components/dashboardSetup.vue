a907f1ee7f5603264106df86d144e5cced96ba8d
William Vitorino <williamvitorino3@gmail.com>
2017-11-09 09:48:49 -0300

    Bootstrap centralizado.

--- protoDashboard/src/components/dashboardSetup.vue
+++ protoDashboard/src/components/dashboardSetup.vue
@@ -1,9 +1,10 @@
 <template>
     <form>
-        <div class="container">
+        <div class="container-fluid">
             <!-- Diminuir o tamanho disso. -->
             <div class="row">
-                <div class="col-xs-10 col-sm-10 col-md-9 col-lg-9">
+                <div :class="bClass.noneClass"></div>
+                <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
                     <div class="form-group">
                         <label>Nome:</label>
                         <input id="nomeDashboardSetup" type="text" placeholder="Nome" class="form-control">
@@ -11,86 +12,97 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-xs-10 col-sm-10 col-md-9 col-lg-9">
-                    <!-- <div class="form-goup"> -->
-                        <label>Select a layout:</label>
-                        <br/>
-                        <span>
-                            <svg stroke-width="0.501" stroke-linejoin="bevel" fill-rule="evenodd" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" version="1.1" overflow="visible" width="161.55pt" height="105.975pt" viewBox="0 0 161.55 105.975">
-                                <g id="Document" fill="none" stroke="black" font-family="Times New Roman" font-size="16" transform="scale(1 -1)">
-                                    <g id="Spread" transform="translate(0 -105.975)">
-                                        <g id="Layer 1">
-                                            <path d="M 0,-0.01 L 0,105.987 L 49.193,105.987 L 49.193,-0.01 L 0,-0.01 Z M 44.807,4.376 L 44.807,101.601 L 4.386,101.601 L 4.386,4.376 L 44.807,4.376 Z" fill-rule="evenodd" stroke="none" stroke-width="0.5" fill="#666666" marker-start="none" marker-end="none" stroke-miterlimit="79.8403193612775"/>
-                                            <path d="M 56.179,-0.01 L 56.179,105.987 L 105.371,105.987 L 105.371,-0.01 L 56.179,-0.01 Z M 100.985,4.376 L 100.985,101.601 L 60.565,101.601 L 60.565,4.376 L 100.985,4.376 Z" fill-rule="evenodd" stroke="none" stroke-width="0.5" fill="#666666" marker-start="none" marker-end="none" stroke-miterlimit="79.8403193612775"/>
-                                            <path d="M 112.357,-0.013 L 112.357,105.984 L 161.55,105.984 L 161.55,-0.013 L 112.357,-0.013 Z M 157.164,4.373 L 157.164,101.598 L 116.743,101.598 L 116.743,4.373 L 157.164,4.373 Z" fill-rule="evenodd" stroke="none" stroke-width="0.5" fill="#666666" marker-start="none" marker-end="none" stroke-miterlimit="79.8403193612775"/>
-                                        </g>
+                <div :class="bClass.noneClass"></div>
+                <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+                    <label>Select a layout:</label>
+                </div>
+            </div>
+            <div class="row">
+                <div :class="bClass.noneClass"></div>
+                <div :class="bClass.svgClass">
+                    <span>
+                        <svg stroke-width="0.501" stroke-linejoin="bevel" fill-rule="evenodd" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" version="1.1" overflow="visible" width="161.55pt" height="105.975pt" viewBox="0 0 161.55 105.975">
+                            <g id="Document" fill="none" stroke="black" font-family="Times New Roman" font-size="16" transform="scale(1 -1)">
+                                <g id="Spread" transform="translate(0 -105.975)">
+                                    <g id="Layer 1">
+                                        <path d="M 0,-0.01 L 0,105.987 L 49.193,105.987 L 49.193,-0.01 L 0,-0.01 Z M 44.807,4.376 L 44.807,101.601 L 4.386,101.601 L 4.386,4.376 L 44.807,4.376 Z" fill-rule="evenodd" stroke="none" stroke-width="0.5" fill="#666666" marker-start="none" marker-end="none" stroke-miterlimit="79.8403193612775"/>
+                                        <path d="M 56.179,-0.01 L 56.179,105.987 L 105.371,105.987 L 105.371,-0.01 L 56.179,-0.01 Z M 100.985,4.376 L 100.985,101.601 L 60.565,101.601 L 60.565,4.376 L 100.985,4.376 Z" fill-rule="evenodd" stroke="none" stroke-width="0.5" fill="#666666" marker-start="none" marker-end="none" stroke-miterlimit="79.8403193612775"/>
+                                        <path d="M 112.357,-0.013 L 112.357,105.984 L 161.55,105.984 L 161.55,-0.013 L 112.357,-0.013 Z M 157.164,4.373 L 157.164,101.598 L 116.743,101.598 L 116.743,4.373 L 157.164,4.373 Z" fill-rule="evenodd" stroke="none" stroke-width="0.5" fill="#666666" marker-start="none" marker-end="none" stroke-miterlimit="79.8403193612775"/>
                                     </g>
                                 </g>
-                            </svg>
-                        </span>
-                        <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" overflow="visible" width="161.55pt" height="106pt"
-                                viewBox="0 0 161.55 106" id="svg6176" sodipodi:docname="2-col.svg" style="overflow:visible;fill-rule:evenodd;stroke-width:0.50099999;stroke-linejoin:bevel"
-                                inkscape:version="0.92.1 r15371">
-                                <g style="overflow:visible;fill-rule:evenodd;stroke-width:0.50099999;stroke-linejoin:bevel" id="g3686" transform="matrix(1.5389468,0,0,1.0018827,27.390897,0.20331935)">
-                                    <g style="font-size:16px;font-family:'Times New Roman';fill:none;stroke:#000000" transform="matrix(0.99223123,0,0,-0.99808441,-17.813751,-0.1886765)"
-                                        font-size="16" id="Document-4">
-                                        <g transform="translate(0,-105.975)" id="Spread-3">
-                                            <g id="Layer 1-6">
-                                                <g style="fill:#666666;fill-rule:evenodd;stroke:none;stroke-width:0.5;stroke-miterlimit:79.84031677" stroke-miterlimit="79.8403193612775"
-                                                    id="Group">
-                                                    <path style="marker-start:none;marker-end:none" inkscape:connector-curvature="0" id="path4501" d="M 0.017,-0.011 V 105.986 H 49.209 V -0.011 Z M 44.823,4.375 V 101.6 H 4.403 V 4.375 Z m 11.793,-4.386 v 105.997 h 49.193 V -0.011 Z m 44.807,4.386 V 101.6 H 61.002 V 4.375 Z"
-                                                        class="teste" />
-                                                </g>
+                            </g>
+                        </svg>
+                    </span>
+                </div>
+                <div :class="bClass.svgClass">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" overflow="visible" width="161.55pt" height="106pt"
+                            viewBox="0 0 161.55 106" id="svg6176" sodipodi:docname="2-col.svg" style="overflow:visible;fill-rule:evenodd;stroke-width:0.50099999;stroke-linejoin:bevel"
+                            inkscape:version="0.92.1 r15371">
+                            <g style="overflow:visible;fill-rule:evenodd;stroke-width:0.50099999;stroke-linejoin:bevel" id="g3686" transform="matrix(1.5389468,0,0,1.0018827,27.390897,0.20331935)">
+                                <g style="font-size:16px;font-family:'Times New Roman';fill:none;stroke:#000000" transform="matrix(0.99223123,0,0,-0.99808441,-17.813751,-0.1886765)"
+                                    font-size="16" id="Document-4">
+                                    <g transform="translate(0,-105.975)" id="Spread-3">
+                                        <g id="Layer 1-6">
+                                            <g style="fill:#666666;fill-rule:evenodd;stroke:none;stroke-width:0.5;stroke-miterlimit:79.84031677" stroke-miterlimit="79.8403193612775"
+                                                id="Group">
+                                                <path style="marker-start:none;marker-end:none" inkscape:connector-curvature="0" id="path4501" d="M 0.017,-0.011 V 105.986 H 49.209 V -0.011 Z M 44.823,4.375 V 101.6 H 4.403 V 4.375 Z m 11.793,-4.386 v 105.997 h 49.193 V -0.011 Z m 44.807,4.386 V 101.6 H 61.002 V 4.375 Z"/>
                                             </g>
                                         </g>
                                     </g>
                                 </g>
-                            </svg>
-                        </span>
-                        <span>
-                            <svg stroke-width="0.501" stroke-linejoin="bevel" fill-rule="evenodd" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" version="1.1" overflow="visible" width="161.55pt" height="105.975pt" viewBox="0 0 161.55 105.975">
-                                <g id="Document" fill="none" stroke="black" font-family="Times New Roman" font-size="16" transform="scale(1 -1)">
-                                    <g id="Spread" transform="translate(0 -105.975)">
-                                        <g id="Layer 1">
-                                            <g id="Group" fill-rule="evenodd" stroke="none" stroke-width="0.5" fill="#666666" stroke-miterlimit="79.8403193612775">
-                                                <path d="M 0.001,-0.01 L 0.001,105.987 L 105.369,105.987 L 105.369,-0.01 L 0.001,-0.01 Z M 100.983,4.376 L 100.983,101.601 L 4.387,101.601 L 4.387,4.376 L 100.983,4.376 Z M 112.358,-0.013 L 112.358,105.984 L 161.55,105.984 L 161.55,-0.013 L 112.358,-0.013 Z M 157.164,4.373 L 157.164,101.598 L 116.744,101.598 L 116.744,4.373 L 157.164,4.373 Z" marker-start="none" marker-end="none"/>
-                                            </g>
+                            </g>
+                        </svg>
+                    </span>
+                </div>
+                <div :class="bClass.svgClass">
+                    <span>
+                        <svg stroke-width="0.501" stroke-linejoin="bevel" fill-rule="evenodd" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" version="1.1" overflow="visible" width="161.55pt" height="105.975pt" viewBox="0 0 161.55 105.975">
+                            <g id="Document" fill="none" stroke="black" font-family="Times New Roman" font-size="16" transform="scale(1 -1)">
+                                <g id="Spread" transform="translate(0 -105.975)">
+                                    <g id="Layer 1">
+                                        <g id="Group" fill-rule="evenodd" stroke="none" stroke-width="0.5" fill="#666666" stroke-miterlimit="79.8403193612775">
+                                            <path d="M 0.001,-0.01 L 0.001,105.987 L 105.369,105.987 L 105.369,-0.01 L 0.001,-0.01 Z M 100.983,4.376 L 100.983,101.601 L 4.387,101.601 L 4.387,4.376 L 100.983,4.376 Z M 112.358,-0.013 L 112.358,105.984 L 161.55,105.984 L 161.55,-0.013 L 112.358,-0.013 Z M 157.164,4.373 L 157.164,101.598 L 116.744,101.598 L 116.744,4.373 L 157.164,4.373 Z" marker-start="none" marker-end="none"/>
                                         </g>
                                     </g>
                                 </g>
-                            </svg>
-                        </span> 
-                        <span>
-                            <svg stroke-width="0.501" stroke-linejoin="bevel" fill-rule="evenodd" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" version="1.1" overflow="visible" width="161.55pt" height="105.975pt" viewBox="0 0 161.55 105.975">
-                                <g id="Document" fill="none" stroke="black" font-family="Times New Roman" font-size="16" transform="scale(1 -1)">
-                                    <g id="Spread" transform="translate(0 -105.975)">
-                                        <g id="Layer 1">
-                                            <g id="Group" fill-rule="evenodd" stroke="none" stroke-width="0.5" fill="#666666" stroke-miterlimit="79.8403193612775">
-                                                <path d="M 56.182,-0.01 L 56.182,105.987 L 161.55,105.987 L 161.55,-0.01 L 56.182,-0.01 Z M 157.164,4.376 L 157.164,101.601 L 60.568,101.601 L 60.568,4.376 L 157.164,4.376 Z M 0,-0.013 L 0,105.984 L 49.193,105.984 L 49.193,-0.013 L 0,-0.013 Z M 44.807,4.373 L 44.807,101.598 L 4.386,101.598 L 4.386,4.373 L 44.807,4.373 Z" marker-start="none" marker-end="none"/>
-                                            </g>
+                            </g>
+                        </svg>
+                    </span> 
+                </div>
+                <div :class="bClass.svgClass">
+                    <span>
+                        <svg stroke-width="0.501" stroke-linejoin="bevel" fill-rule="evenodd" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" version="1.1" overflow="visible" width="161.55pt" height="105.975pt" viewBox="0 0 161.55 105.975">
+                            <g id="Document" fill="none" stroke="black" font-family="Times New Roman" font-size="16" transform="scale(1 -1)">
+                                <g id="Spread" transform="translate(0 -105.975)">
+                                    <g id="Layer 1">
+                                        <g id="Group" fill-rule="evenodd" stroke="none" stroke-width="0.5" fill="#666666" stroke-miterlimit="79.8403193612775">
+                                            <path d="M 56.182,-0.01 L 56.182,105.987 L 161.55,105.987 L 161.55,-0.01 L 56.182,-0.01 Z M 157.164,4.376 L 157.164,101.601 L 60.568,101.601 L 60.568,4.376 L 157.164,4.376 Z M 0,-0.013 L 0,105.984 L 49.193,105.984 L 49.193,-0.013 L 0,-0.013 Z M 44.807,4.373 L 44.807,101.598 L 4.386,101.598 L 4.386,4.373 L 44.807,4.373 Z" marker-start="none" marker-end="none"/>
                                         </g>
                                     </g>
                                 </g>
-                            </svg>
-                        </span>
-                        <span>
-                            <svg stroke-width="0.501" stroke-linejoin="bevel" fill-rule="evenodd" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" version="1.1" overflow="visible" width="161.1pt" height="105.975pt" viewBox="0 0 161.1 105.975">
-                                <g id="Document" fill="none" stroke="black" font-family="Times New Roman" font-size="16" transform="scale(1 -1)">
-                                    <g id="Spread" transform="translate(0 -105.975)">
-                                        <g id="Layer 1">
-                                            <path d="M 0.012,-0.011 L 0.012,105.986 L 161.088,105.986 L 161.088,-0.011 L 0.012,-0.011 Z M 156.702,4.375 L 156.702,101.6 L 4.398,101.6 L 4.398,4.375 L 156.702,4.375 Z" stroke-width="0.5" stroke="none" fill="#666666" fill-rule="evenodd" marker-start="none" marker-end="none" stroke-miterlimit="79.8403193612775"/>
-                                        </g>
+                            </g>
+                        </svg>
+                    </span>
+                </div>
+                <div :class="bClass.svgClass">
+                    <span>
+                        <svg stroke-width="0.501" stroke-linejoin="bevel" fill-rule="evenodd" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" version="1.1" overflow="visible" width="161.1pt" height="105.975pt" viewBox="0 0 161.1 105.975">
+                            <g id="Document" fill="none" stroke="black" font-family="Times New Roman" font-size="16" transform="scale(1 -1)">
+                                <g id="Spread" transform="translate(0 -105.975)">
+                                    <g id="Layer 1">
+                                        <path d="M 0.012,-0.011 L 0.012,105.986 L 161.088,105.986 L 161.088,-0.011 L 0.012,-0.011 Z M 156.702,4.375 L 156.702,101.6 L 4.398,101.6 L 4.398,4.375 L 156.702,4.375 Z" stroke-width="0.5" stroke="none" fill="#666666" fill-rule="evenodd" marker-start="none" marker-end="none" stroke-miterlimit="79.8403193612775"/>
                                     </g>
                                 </g>
-                            </svg>
-                        </span>
-                    <!-- </div> -->
+                            </g>
+                        </svg>
+                    </span>
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-xs-10 col-sm-10 col-md-9 col-lg-9">
+                <div :class="bClass.noneClass"></div>
+                <div :class="bClass.dropdownsClass">
                     <div class="form-group">
                         <label>
                             Team:
@@ -103,9 +115,7 @@
                         </select>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-10 col-sm-10 col-md-9 col-lg-9">
+                <div :class="bClass.dropdownsClass">
                     <div class="form-group">
                         <label>
                             Team Members:
@@ -116,7 +126,8 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-xs-10 col-sm-10 col-md-9 col-lg-9">
+                <div :class="bClass.noneClass"></div>
+                <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
                     <button class="btn btn-primary">Create</button>
                     <button class="btn btn-primary">Cancel</button>    
                 </div>
@@ -128,15 +139,24 @@
 
 <script>
 export default {
+    data() {
+        return {
+            bClass: {
+                svgClass: 'col-xs-2 col-sm-2 col-md-2 col-lg-2',
+                noneClass: 'col-xs-1 col-sm-1 col-md-1 col-lg-1',
+                dropdownsClass: 'col-xs-5 col-sm-5 col-md-5 col-lg-5'
+            },
+        }
+    }
   
 }
 </script>
 
 <style scoped>
 svg {
-    width: 10%;
-    margin-left: 2%;
-    margin-right: 2%;
+    width: 75%;
+    /* margin-left: 20%;
+    margin-right: 20%; */
 }
 label {
     text-align: left;
