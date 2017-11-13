4ce2b56556cfd8db76ad61705adfb04b78b92155
jacianabarbosas <jacianabarbosas@gmail.com>
2017-11-13 09:51:43 +0000

    Arquivo dashboards.vue adicionado.

---
+++ protoDashboard/src/components/dashboards.vue
@@ -0,0 +1,62 @@
+
+<template>
+    <div class="container">
+      <div class="row">
+        <div class="text-left col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <h1>Dashboards</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-11 col-sm-11 col-md-11 col-lg-11">
+          <div class="btn-group" role="group">
+            <button class="btn btn-default" v-for="botao in botoes">{{botao}}</button>
+            <button class="btn btn-success">+</button>
+          </div>
+        </div>
+        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" >
+          <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="addChart col-xs-3 col-sm-3 col-md-3 col-lg-3">
+            <span>+ Add Chart</span>
+        </div>
+        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+        <div class="addChart col-xs-3 col-sm-3 col-md-3 col-lg-3">
+            <span>+ Add Chart</span>
+        </div>
+        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+        <div class="addChart col-xs-3 col-sm-3 col-md-3 col-lg-3">
+            <span>+ Add Chart</span>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default{
+      data() {
+        return{
+          botoes: ["Visao Geral", "vendas", "botao", "a"]
+        }
+      }
+    }
+</script>
+
+<style scoped>
+.addChart{
+  padding-top: 10%;
+  padding-bottom: 10%;
+  margin: 0% 1% 0% 1%;
+  border-style: dashed;
+  border-radius: 5px;
+  border-color: gray;
+}
+.row {
+  margin-bottom: 2%;
+}
+
+.{
+  background-color: blue;
+}
+</style>
